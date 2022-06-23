e5c8f41c5e98440799f300c4108163fb123b4a4a
Michell Sosa <michellsosa89@gmail.com>
2022-06-22 20:31:50 -0600

    commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://localhost:27017/testdb').then(() => {
     console.log("connection failed");
 })
 
-const port = 8080;
+let port = process.env.PORT || 3000;
 const protectedRoute = express.Router();
 
 app.set('key', 'secret');
